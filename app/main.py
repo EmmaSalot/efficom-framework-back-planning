671714dbc2de972f83448ccb53345af57d00fe79
Benoit <blavoine@icloud.com>
2024-03-05 10:42:48 +0100

    Import database connnec

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from database import get_database
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    return {"Hello": "World"}
+
+@app.get("/users/")
+def add_user():
+    
+    
     return {"Hello": "World"}
