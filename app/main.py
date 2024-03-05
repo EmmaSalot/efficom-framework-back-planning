b8e6ad8948738db8d364f323346fe8806459b46d
Benoit <blavoine@icloud.com>
2024-03-05 16:10:44 +0100

    We can now call companies api + bugfix

--- app/main.py
+++ app/main.py
@@ -4,10 +4,13 @@ from pydantic import BaseModel
 
 #local imports
 from routers.users import router as user_router
+from routers.companies import router as company_router
 from database import initialize_database
+from database import get_users_collection
 
 app = FastAPI()
 app.include_router(user_router, tags=["Users"])
+app.include_router(company_router, tags=["Companies"])
 
 # Initialize the database connection
 initialize_database() 
--- app/models/companies.py
+++ app/models/companies.py
@@ -5,13 +5,13 @@ from __future__ import annotations
 from pydantic import BaseModel
 
 class CreateCompany(BaseModel):
-    name: str
-    address : str
-    users : list[CreateUser]
-    planning : list[CreateActivity]
+    name: str 
+    address : str | None = None
+    users : list[CreateUser] | None = None
+    planning : list[CreateActivity] | None = None
 
 class Company(CreateCompany):
-    id: int
+    _id: int | None = None
 
 #local imports (here to avoid circular import problems)
 from models.users import CreateUser
